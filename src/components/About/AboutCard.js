75c7fdcb5775d6a895e2d960d7be0e8f60a523ea
redhatn16 <redhat1116@gmail.com>
2023-08-01 02:00:22 +0800

    refactor name and from

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">{process.env.REACT_APP_HERO_NAME ?? ""} </span>
+            from <span className="purple"> {process.env.REACT_APP_HERO_FROM ?? ""}.</span>
             <br /> I am a final year student pursuing an Integrated MSc (IMSc)
             in Maths and Computing at BIT Mesra.
             <br />
